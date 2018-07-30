e04acc6772e88a5e98708e4efc9199cc0031887b
AlexDuOl <contactogatovagabundo@gmail.com>
2018-07-30 15:18:10 -0500

    Subiendo funcion, ajustes en index.html y cambios en css

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-window.addEventListener('load', init);
+let dragImg = document.getElementsByClassName("collection");
+let dropBox = document.getElementsByClassName("collageImg");
+let dragged = null;
+
+//Entrando al array de imagenes
+Array.from(dragImg).forEach((img) => {
+    img.ondragstart = function (e) {
+        dragged = e.target;
+        console.log("Esta drageando");
+    }
+});
+
+//Entrando al array del div para colocar las imagenes
+Array.from(dropBox).forEach((box) => {
+    box.ondragover = function (e) {
+        e.preventDefault();
+        console.log("drag");       
+    }
+    box.ondrop = function (e) {
+        e.preventDefault();
+        box.appendChild(dragged);
+        console.log(dragged);
+    }
+
+});
+
+//.setAttribute("x", e);
+
